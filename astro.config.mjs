a403cfde012a5ffe0c1264eb74d253033a037ede
Prass, The Nomadic Coder <me@prassanna.io>
2025-03-06 02:28:56 +0000

    🔧 update sitemap base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://prassanna-ravishankar.github.io',
+  site: 'https://prassanna.io',
   
   integrations: [
     tailwind(),
